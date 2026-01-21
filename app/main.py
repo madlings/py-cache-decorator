fe062d0599a49e40f262f98fa7fa292e600dfcb3
madlings <nagalingam.s@gmail.com>
2026-01-21 13:17:28 +0530

    Update main.py

--- app/main.py
+++ app/main.py
@@ -1,6 +1,18 @@
 from typing import Callable
+import functools
 
 
 def cache(func: Callable) -> Callable:
-    # Write your code here
-    pass
+    storage = {}
+
+    @functools.wraps(func)
+    def wrapper(*args, **kwargs) -> Callable:
+        if args in storage:
+            print("Getting from cache")
+            return storage[args]
+        else:
+            print("Calculating new result")
+            result = func(*args, **kwargs)
+            storage[args] = result
+            return result
+    return wrapper
